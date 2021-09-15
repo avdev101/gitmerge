26b1bc8b59c7effdef32d25346b0a5c5d3df21c6
aleksey <eremeev.crypt@gmail.com>
2021-09-15 14:11:26 +0300

    start server

--- cmd/gitmerge/main.go
+++ cmd/gitmerge/main.go
@@ -1,10 +1,25 @@
 package main
 
 import (
+	"eremeev/gitmerge/adapters"
+	"eremeev/gitmerge/core"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("start...")
+
+	store := adapters.GitlabStore{}
+
+	mergeService := core.MergeService{
+		MergeStore: store,
+	}
+
+	server := adapters.Server{
+		MergeService: mergeService,
+		Port:         9191,
+	}
+
+	server.Start()
 
 }
