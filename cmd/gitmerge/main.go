e0c6521ade9f056cbf5519529d27ae07c8e950fe
aleksey <eremeev.crypt@gmail.com>
2021-09-27 22:07:32 +0300

    fix GitlabStore create args

--- cmd/gitmerge/main.go
+++ cmd/gitmerge/main.go
@@ -6,10 +6,16 @@ import (
 	"fmt"
 )
 
+var BASE_PATH = "https://gitlab.com/api/v4"
+var TOKEN = "_NA2XQAwwJDksNKxYLzE"
+
 func main() {
 	fmt.Println("start...")
 
-	store := adapters.GitlabStore{}
+	store := adapters.GitlabStore{
+		Token:    TOKEN,
+		BasePath: BASE_PATH,
+	}
 
 	mergeService := core.MergeService{
 		MergeStore: store,
