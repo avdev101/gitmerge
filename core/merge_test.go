54f7df651de449d32367fce2f72440bd4403738c
aleksey <eremeev.crypt@gmail.com>
2022-04-05 23:55:10 +0300

    update test

--- core/merge_test.go
+++ core/merge_test.go
@@ -1,6 +1,9 @@
 package core
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestGetIssueId(t *testing.T) {
 	merge := MergeRequest{
@@ -14,6 +17,23 @@ func TestGetIssueId(t *testing.T) {
 
 	if issueId != "123" {
 		t.Errorf("expect %v get %v", 123, issueId)
+	}
+}
+
+func TestLinkIssue(t *testing.T) {
+	merge := MergeRequest{
+		ProjectID:    1,
+		ID:           2,
+		Description:  "description",
+		SourceBranch: "feature/issue/123",
+	}
+
+	expected := fmt.Sprintf("#%v\n\n%v", 123, merge.Description)
+
+	merge.LinkIssue()
+
+	if merge.Description != expected {
+		t.Errorf("desc: expected: %s, get %s", expected, merge.Description)
 	}
 }
 
